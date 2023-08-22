eee4bd626b9198cbe015b9b447e57c5c7584edce
ashutosh3216 <118066808+ashutosh3216@users.noreply.github.com>
2023-08-22 23:02:33 +0530

    initial commit

---
+++ src/main/java/com/ashutosh/spring/redis/repository/TutorialRepository.java
@@ -0,0 +1,15 @@
+package com.ashutosh.spring.redis.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ashutosh.spring.redis.model.Tutorial;
+
+@Repository
+public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
+  List<Tutorial> findByPublished(boolean published);
+
+  List<Tutorial> findByTitleContaining(String title);
+}
